531933aba5abfc24653c1deab766bb09989d962b
tsaitsetu-dev <tsaitsetu@gmail.com>
2026-02-10 20:59:52 -0500

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css';
+
+export const metadata = {
+  title: 'Squad Shop',
+  description: 'Plan group shopping trips in NYC',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
